6740397aea0a5bdb6154fb91f85c013b60422a5e
Bharati Gogoi <bharatigogoi@Bharatis-MacBook-Air.local>
2026-06-23 01:45:05 +0530

    [fix][Added Comment] -  Demo on How to use the Component, will be converting this project into a package in next releases.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import "./App.css";
 import SVGViewer from "./pages/SVGViewer";
 
+
 function App() {
   return (
     <div style={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
+
+      {/* Demo on How to use the Component, will be converting this project into a package in next releases. */}
       <SVGViewer>
         <svg viewBox="0 0 100 50" xmlns="http://www.w3.org/2000/svg">
           <circle cx="50" cy="25" r="20" />
